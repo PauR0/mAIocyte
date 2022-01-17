8970d74d3b5d9553dfbe596620051339558ac0a2
commlab <commlab@olivia.uv.es>
2022-01-17 13:14:23 +0100

    Added shell scripts to run elvira simulations

---
+++ runelvBZ.sh
@@ -0,0 +1,4 @@
+SOFT_PATH=/home/commlab/electrophys/ElviraBZ
+
+mpirun -n $1 $SOFT_PATH/bin/mainelv_openmpi_gcc_bz -i $2 -o $3 > runelv.out
+touch endelv.dat
---
+++ runpost.sh
@@ -0,0 +1,1 @@
+/home/commlab/electrophys/PostElvira/bin/postelv_gcc -i $1 -o $2 > runpost.out
