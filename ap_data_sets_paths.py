8dc84d66548b4c4b315802cf058b8fcf3fafe49f
Pau Romero <pau.romero@uv.es>
2022-04-28 13:00:38 +0200

    File with paths info

---
+++ ap_data_sets_paths.py
@@ -0,0 +1,53 @@
+
+import sys
+
+#The directory where this file is located
+#FILE_DIR = sys.path[0]
+FILE_DIR = '/Users/pau/Electrophys/StemCells'
+#--------------------------------------------------------
+#EPI AP curves data sets
+#--------------------------------------------------------
+EPI_HD_PATH  = FILE_DIR+"/AP_DS/EPI_AP_DS.pickle"
+EPI_P8_PATH  = FILE_DIR+"/AP_DS/EPI_AP_P8.pickle"
+EPI_P80_PATH = FILE_DIR+"/AP_DS/EPI_LD_P80.pickle"
+#--------------------------------------------------------
+#########################################################
+#--------------------------------------------------------
+#EPI_BZ AP curves data sets
+#--------------------------------------------------------
+EPI_BZ_HD_PATH  = FILE_DIR+"/AP_DS/EPI_BZ_AP_DS.pickle"
+EPI_BZ_P8_PATH  = FILE_DIR+"/AP_DS/EPI_BZ_AP_P8.pickle"
+EPI_BZ_P80_PATH = FILE_DIR+"/AP_DS/EPI_BZ_LD_P80.pickle"
+#--------------------------------------------------------
+#########################################################
+#--------------------------------------------------------
+#ENDO AP curves data sets
+#--------------------------------------------------------
+ENDO_HD_PATH  = FILE_DIR+"/AP_DS/ENDO_AP_DS.pickle"
+ENDO_P8_PATH  = FILE_DIR+"/AP_DS/ENDO_AP_P8.pickle"
+ENDO_P80_PATH = FILE_DIR+"/AP_DS/ENDO_LD_P80.pickle"
+#--------------------------------------------------------
+#########################################################
+#--------------------------------------------------------
+#ENDO_BZ AP curves data sets
+#--------------------------------------------------------
+ENDO_BZ_HD_PATH  = FILE_DIR+"/AP_DS/ENDO_BZ_AP_DS.pickle"
+ENDO_BZ_P8_PATH  = FILE_DIR+"/AP_DS/ENDO_BZ_AP_P8.pickle"
+ENDO_BZ_P80_PATH = FILE_DIR+"/AP_DS/ENDO_BZ_LD_P80.pickle"
+#--------------------------------------------------------
+#########################################################
+#--------------------------------------------------------
+#MID AP curves data sets
+#--------------------------------------------------------
+MID_HD_PATH  = FILE_DIR+"/AP_DS/MID_AP_DS.pickle"
+MID_P8_PATH  = FILE_DIR+"/AP_DS/MID_AP_P8.pickle"
+MID_P80_PATH = FILE_DIR+"/AP_DS/MID_LD_P80.pickle"
+#--------------------------------------------------------
+#########################################################
+#--------------------------------------------------------
+#MID_BZ AP curves data sets
+#--------------------------------------------------------
+MID_BZ_HD_PATH  = FILE_DIR+"/AP_DS/MID_BZ_AP_DS.pickle"
+MID_BZ_P8_PATH  = FILE_DIR+"/AP_DS/MID_BZ_AP_P8.pickle"
+MID_BZ_P80_PATH = FILE_DIR+"/AP_DS/MID_BZ_LD_P80.pickle"
+#--------------------------------------------------------
