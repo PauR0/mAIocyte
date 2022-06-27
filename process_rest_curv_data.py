2cb724ffa84020ede6a2484d6b1c06881b7e2ca8
Pau Romero <pau.romero@uv.es>
2022-06-27 15:51:53 +0200

    Split APD and CV computations and added to argparse

--- process_rest_curv_data.py
+++ process_rest_curv_data.py
@@ -1,12 +1,13 @@
 #!usr/bin/env/python3
 
-import os
+import os, sys
 
 import argparse
 
 import numpy as np
 
 from scipy.optimize import curve_fit
+from scipy.stats import linregress
 
 import pandas as pd
 
@@ -210,12 +211,16 @@ def make_APDR_curve(data, fname=None, debug=False, w=False):
 
     #p0 iteration was neded in some cases to converge, however it may a local minimum :|
     abc, _ = curve_fit(exp_, x, y, p0=[389.02417036, 0.5086576, 112.45328555])
-    xx = np.linspace(min(5, x.min()), max(x.max(), 700), 200)
+    xx = np.linspace( x.min(), x.max(), 200)
 
     if debug:
-        print("a b c: ", abc )
-        plt.plot(x, y, 'bo')
-        plt.plot(xx, exp_(xx, *abc), 'k-')
+        res = y - exp_(x, *abc)
+        print("APD exp params: ", abc )
+        print(f"Residuals: mean {res.mean()}, std {res.std()}, max {res.max()}")
+        _, [ax1, ax2] = plt.subplots(1,2, sharex=True)
+        ax1.scatter(x, y, label='samples')
+        ax1.plot(xx, exp_(xx, *abc), 'k-', label='model')
+        ax2.plot(x, res, 'r', label='residuals')
         plt.show()
 
 
@@ -228,16 +233,21 @@ def make_APDR_curve(data, fname=None, debug=False, w=False):
 
 def make_CVR_curve(data, fname=None, debug=False, s=10, w=False):
 
-    if os.path.exists(fname) and not w:
-        print(f"Warning {fname} already exists and overwitting option is set to False. Nothing will be saved....")
-        return False
+    """"
+    if fname:
+        if os.path.exists(fname) and not w:
+            print(f"Warning {fname} already exists and overwitting option is set to False. Nothing will be saved....")
+            return False
+    """
 
     data['DI'] = data[['DI_or', 'DI_dest']].mean(axis=1)
-    data = data.drop(data[(data.DI < 27) & (data['CV'] > 0.04)].index)
-    data = data.drop(data[data.CV < 0].index)
-    data = data.drop(data[(data.DI < 30) & (data['CV'] > 0.045)].index)
-    data_df = data.drop_duplicates(subset='DI', keep="last")
-    data_df = data_df.sort_values('DI')
+    data['CV'] = data['CV'].abs()
+    #data = data.drop(data[(data.DI < 27) & (data['CV'] > 0.04)].index)
+    #data = data.drop(data[data.CV < 0].index)
+    #data = data.drop(data[(data.DI < 30) & (data['CV'] > 0.045)].index)
+    #data_df = data.drop_duplicates(subset='DI', keep="last")
+    #data_df = data_df.sort_values('DI')
+    data_df = data
 
     x = data_df['DI'].to_numpy()
     y = data_df['CV'].to_numpy()
@@ -249,7 +259,7 @@ def make_CVR_curve(data, fname=None, debug=False, s=10, w=False):
     if debug:
         print("a b c: ", abc )
         _, ax = plt.subplots()
-        sns.scatterplot(x='DI', y='CV', hue='node_dest', style='node_or', data=data_df, ax=ax)
+        sns.scatterplot(x='DI', y='CV', hue='S1', data=data_df, ax=ax)
         ax.plot(xx, exp_(xx, *abc), 'k-')
         plt.show()
 
@@ -290,26 +300,90 @@ def make_APDR_surface(data, fname=None, debug=False, w=False):
     di    = data['DI'].to_numpy()
     apd_1 = data['APD+1'].to_numpy()
 
-    print("APD", apd.min(), apd.max())
-    print("DI", di.min(), di.max())
+    abc = make_APDR_curve(data, debug=False)
+
+    apd_1_diff = apd_1 - exp_(di, *abc)
+    linreg = linregress(apd, apd_1_diff)
+
+    aapdd = np.linspace(apd.min(), apd.max(), 100)
+    ddii = np.linspace(di.min(), di.max(), 100)
+
 
     if debug:
+        print("APDRS exp params: ", abc )
+        print(f"APDRS linear params: slope {linreg.slope} intrcpt {linreg.intercept} r2 {linreg.rvalue**2} p value {linreg.pvalue}")
+        #The p-value for a hypothesis test whose null hypothesis is that the slope is zero, using Wald Test with t-distribution of the test statistic
+
+        X, Y = np.meshgrid(ddii, aapdd)
+        Z = exp_(X, *abc) + (linreg.slope * Y + linreg.intercept)
+
         fig = plt.figure()
-        ax0 = fig.add_subplot(121)
-        ax1 = fig.add_subplot(122, projection='3d')
-        ax1.scatter(apd, di, apd_1, c=apd_1)
+        ax0 = fig.add_subplot(131)
+        ax1 = fig.add_subplot(132, projection='3d')
+        ax2 = fig.add_subplot(133)
         ax0.scatter(di, apd_1, c=apd_1)
+        ax0.plot(ddii, exp_(ddii, *abc), 'k')
+
+        ax1.scatter(di, apd, apd_1)#, c=data['S1'])
+        ax1.plot_surface(X, Y, Z, linewidth=0, cmap='viridis', antialiased=False, alpha=0.5)
+
+        sns.scatterplot(x='APD', y=apd_1_diff, data=data, ax=ax2, hue='S1', legend=True)#palette='viridis',
+        ax2.scatter(aapdd, (linreg.slope * aapdd + linreg.intercept))
         plt.show()
 
+    if fname:
+        save_arr = np.zeros( (ddii.shape[0]+1, aapdd.shape[0]+1))
+        save_arr[0,1:] = ddii
+        save_arr[1:,0] = aapdd
+        save_arr[1:,1:] = [exp_(d, *abc) + (linreg.slope * aapdd + linreg.intercept) for d in ddii]
+
+        save_arr = save_arr.astype(str)
+        np.savetxt(fname, save_arr, fmt="\"%s\"", delimiter=',')
+#
+
 def make_CVR_surface(data, fname=None, debug=False, w=False):
-    pass
 
-def compute_restitution_surfaces(path, cell_type=None, bz=False, w=False, debug=False):
 
-    APD_df, CV_df = load_data(path)
+    data['DI'] = data[['DI_or', 'DI_dest']].mean(axis=1)
+    #apd  = data['APD'].to_numpy()
+    di  = data['DI'].to_numpy()
+    cv  = data['CV'].abs().to_numpy()
 
-    make_APDR_surface(data=APD_df, debug=debug, w=w)
+    abc, _ = curve_fit(exp_, xdata=di, ydata=cv, p0=[0.07130694, 0.91347783, 52.24539094])
 
+    cv_res = cv - exp_(di, *abc)
+    #linreg = linregress(apd, cv_res)
+    ddii = np.linspace(di.min(), di.max(), 200)
+    #aapdd = np.linspace(apd.min(), apd.max(), 200)
+
+
+    if debug:
+
+        #X, Y = np.meshgrid(ddii, aapdd)
+        #Z = exp_(X, *abc) + (linreg.slope * Y + linreg.intercept)
+
+        fig = plt.figure()
+        ax0 = fig.add_subplot(121)
+        #ax1 = fig.add_subplot(132, projection='3d')
+        ax2 = fig.add_subplot(122)
+
+        ax0.scatter(di, cv, s=5)
+        ax0.plot(ddii, exp_(ddii, *abc), 'k')
+
+        #ax1.plot_surface(X, Y, Z, linewidth=0, cmap='viridis', antialiased=False, alpha=0.5)
+
+        sns.scatterplot(x='S1', y=cv_res, hue='S1', data=data, ax=ax2)
+        #ax2.scatter(aapdd, (linreg.slope * aapdd + linreg.intercept))
+        plt.show()
+#
+
+def compute_restitution_surfaces(path, cell_type=None, w=False, debug=False):
+
+    APD_data, CV_data = load_data(path)
+
+    make_APDR_surface(data=APD_data, debug=debug, w=w)
+    make_CVR_surface(data=CV_data, debug=debug, w=w)
+#
 
 if __name__ == '__main__':
 
@@ -319,15 +393,24 @@ if __name__ == '__main__':
     parser.add_argument('-m',
                         '--myo',
                         dest='myo',
-                        default='ttendo',
+                        default='',
                         type=str,
-                        help="""Flag to specify if ttepi - ttmid - ttendo. Default ttendo.""")
+                        help="""Flag to specify the cell model, must be in {ttepi, ttmid, ttendo, ttepibz, ttmidbz, ttendobz}""")
 
-    parser.add_argument('-b',
-                        '--border-zone',
-                        dest='bz',
+    parser.add_argument('--APDRC',
+                        dest='APDRC',
                         action='store_true',
-                        help="""Set the node type to Border Zone.""")
+                        help=""" Generate APD restiturion curve.""")
+
+    parser.add_argument('--APDRS',
+                        dest='APDRS',
+                        action='store_true',
+                        help="""Generate APD restiturion surface.""")
+
+    parser.add_argument('--CVRC',
+                        dest='CVRC',
+                        action='store_true',
+                        help="""Generate conduction velocity restiturion curve.""")
 
     parser.add_argument('-w',
                         '--overwrite',
@@ -350,5 +433,38 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    #compute_restitution_curves(path=args.path, cell_type=args.myo, bz=args.bz, w=args.w, debug=args.debug)
-    compute_restitution_surfaces(path=args.path, cell_type=args.myo, bz=args.bz, w=args.w, debug=args.debug)
+    APD=None
+    CV=None
+
+    if args.myo.startswith('ttendo'):
+        cell_type = 'Endo'
+    elif args.myo.startswith('ttepi'):
+        cell_type = 'Epi'
+    elif args.myo.startswith('ttmid'):
+        cell_type = 'Mid'
+    else:
+        print("WARNING --myo argument could not be understood...")
+        sys.exit(1)
+
+    s=10
+    aux='Sanas'
+    if 'bz' in args.myo.lower():
+        s*=0.25
+        aux='BZ'
+
+    if args.APDRC:
+        APD = load_APD_data(path=args.path, ext='csv', rebuild=False, w=args.w)
+        fname = f"{args.path}/Rest_Curve_data/RestitutionCurve_{aux}_APD_{cell_type}.csv"
+        make_APDR_curve(data=APD, fname=fname, debug=args.debug, w=args.w)
+
+    if args.APDRS:
+        if APD is None:
+            APD = load_APD_data(path=args.path, ext='csv', rebuild=False, w=args.w)
+        fname = f"{args.path}/Rest_Curve_data/RestitutionSurface_{aux}_APD_{cell_type}.csv"
+        make_APDR_surface(data=APD, fname=fname, debug=args.debug, w=args.w)
+
+    if args.CVRC:
+        CV = load_CV_data(path=args.path, ext='csv', rebuild=False, w=args.w)
+        fname = f"{args.path}/Rest_Curve_data/RestitutionCurve_{aux}_CV_{cell_type}.csv"
+        make_CVR_curve(CV, fname=fname, debug=args.debug, s=s, w=args.w)
+#
