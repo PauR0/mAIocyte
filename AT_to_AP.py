56a9d3cb1657b4c9c7d4ad87a3b3cf18c7801632
Pau Romero <pau.romero@uv.es>
2023-03-31 11:54:33 +0200

    Redesigned Script to adapt it to new case paradygm

--- AT_to_AP.py
+++ AT_to_AP.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 
 import os, sys
+import re
+import argparse
 import datetime as dt
+
 import numpy as np
 import pyvista as pv
 from tqdm import tqdm
@@ -9,8 +12,9 @@ from tqdm import tqdm
 from arrithmic3D_case_reader import A3DENS
 from CellSim import CellSim
 from param_utils import read_AP_json, write_AP_json
+from utils import pad_list_to_array
 
-def load_cell_types_from_mesh(fname):
+def load_cell_types_from_mesh(case_dir):
 
     """
         'EndoToEpi':
@@ -24,7 +28,7 @@ def load_cell_types_from_mesh(fname):
             2 : core
     """
 
-    mesh = pv.read(fname)
+    mesh = pv.read(f"{case_dir}/ventricle_Tagged.vtk")
     cell_types = np.empty( (mesh.points.shape[0],), dtype=object)
     cell_types[mesh['EndoToEpi']==0] = 'ttendo'
     cell_types[mesh['EndoToEpi']==1] = 'ttmid'
@@ -51,19 +55,65 @@ def ens_to_act_times(ens):
     return np.array(ens.fields['Activation_Map(ms)']['data'])
 #
 
-def AT_to_AP(enscase_fname,
-             mesh_fname,
-             ap_params):
+def load_act_times(act_times_file):
 
+    path, name_ext = os.path.split(act_times_file)
+    name, ext = os.path.splitext(name_ext)
 
-    path, _ = os.path.split(enscase_fname)
+    if ext.lower() == '.case':
+        ens = A3DENS()
+        ens.read_ens_case_file(act_times_file)
+        ens.load_variables(var='Activation_Map(ms)')
+        act_times = ens_to_act_times(ens).T
 
-    ens = A3DENS()
-    ens.read_ens_case_file(enscase_fname)
-    ens.load_variables(var='Activation_Map(ms)')
-    act_times = ens_to_act_times(ens)
+    elif ext == '.txt':
+        with open(act_times_file,'r') as f:
+            f = f.read()
 
-    cell_types = load_cell_types_from_mesh(mesh_fname)
+        f_rep = re.sub(r'FloatList\s+size\=\d+\s', '', f).splitlines()
+        lat = [eval(l) for l in tqdm(f_rep, desc="Parsing activation times.")]
+        act_times = pad_list_to_array(lat)
+
+    elif ext.lower() == '.npy':
+        act_times = np.load(act_times_file)
+
+    return act_times
+#
+
+def make_sim_dir(case_dir, act_times_file=None, sim_id=None, f=False):
+
+    if sim_id is None:
+        if act_times_file is not None:
+            name, _ = os.path.splitext(act_times_file)
+            i = name.rfind('_')+1
+            sim_id = name[i:]
+        else:
+            now = dt.datetime.now()
+            sim_id = now.strftime('%d_%m_%Y')
+
+    sim_dir = f"{case_dir}/{sim_id}/"
+
+    if os.path.exists(sim_dir) and not f:
+        print(f"ERROR: Directory {sim_dir} already exists. To overwrite it try with -f option.")
+        return None, None
+    else:
+        os.makedirs(sim_dir, exist_ok=True)
+        return sim_dir, sim_id
+
+def AT_to_AP(case_dir,
+             ap_params,
+             act_times_file,
+             f=False):
+
+    act_times = load_act_times(act_times_file=act_times_file)
+    at_ids = ~ np.isnan(act_times)
+
+    print(f"ACT_TIMES:  shape {act_times.shape}; max {act_times[at_ids].max()}; min {act_times[at_ids].min()}; nans {(~at_ids).sum()}")
+
+    cell_types = load_cell_types_from_mesh(case_dir)
+    print(f"Cell types description:")
+    for ct in np.unique(cell_types):
+        print(f"\- Type {ct}: n {(cell_types == ct).sum()}")
     cell_models = build_cell_models(np.unique(cell_types))
 
     t_ini = ap_params['data']['t_ini']
@@ -71,7 +121,7 @@ def AT_to_AP(enscase_fname,
     t_extra = ap_params['data']['t_extra']
 
     if ap_params['data']['t_end'] is None:
-        t_end = act_times.max() + t_extra
+        t_end = act_times[at_ids].max() + t_extra
         ap_params['data']['t_end'] = t_end
     N = int((t_end-t_ini)/t_delta)
     times = np.linspace(t_ini, t_end, N)
@@ -81,9 +131,18 @@ def AT_to_AP(enscase_fname,
     ap_params['metadata']['date'] = now.strftime('%d-%m-%Y')
     ap_params['metadata']['time'] = now.strftime('%H:%M')
     ap_params['data']['shape'] = (len(cell_types), times.shape[0])
-    write_AP_json(path, data=ap_params)
 
-    AP = np.memmap(f"{path}/AP.npy", dtype='float64', mode='w+', shape=(len(cell_types), times.shape[0]) )
+    sim_dir, sim_id = make_sim_dir(case_dir=case_dir, act_times_file=act_times_file, sim_id=ap_params['data']['sim_id'], f=f)
+
+    if sim_dir is None:
+        return
+
+    if ap_params['data']['sim_id'] is None:
+        ap_params['data']['sim_id'] = sim_id
+
+    write_AP_json(sim_dir, data=ap_params)
+    np.save(f"{sim_dir}/act_times.npy", act_times)
+    AP = np.memmap(f"{sim_dir}/AP.npy", dtype='float64', mode='w+', shape=(len(cell_types), times.shape[0]))
     for nid, ct in enumerate(tqdm(cell_types)):
         if ct != 'core':
             cell = cell_models[ct]
@@ -97,11 +156,69 @@ def AT_to_AP(enscase_fname,
 
 if __name__ == '__main__':
 
-    case_dir = "/Users/pau/Electrophys/Automata/p2_Bivent"
-    ens_dir  = "A_Map_Berruezo_p2_Bivent_IDPacing-39016_6S1-600_1S2-285"
-    enscase_fname = f"{case_dir}/{ens_dir}/VentA_Map_Berruezo_p2_Bivent.case"
-    mesh_fname = f"/Users/pau/Electrophys/Automata/p2_Bivent/ventricle_Tagged.vtk"
+    parser = argparse.ArgumentParser(description="""Script to generate the Action potetials
+                                                    from a simulated case. """)
 
-    ap_params = read_AP_json()
+    parser.add_argument('-f',
+                        '--force',
+                        dest='f',
+                        action='store_true',
+                        help=""" Overwrite existing files.""")
 
-    AT_to_AP(enscase_fname=enscase_fname, mesh_fname=mesh_fname, ap_params=ap_params)
+    parser.add_argument('-d',
+                        '--debug',
+                        dest='debug',
+                        action='store_true',
+                        help="""Plot the available data and print stuff.""")
+
+    parser.add_argument('-a',
+                        '--act-times-file',
+                        dest='act_times',
+                        action='store',
+                        default=None,
+                        type=str,
+                        nargs='?',
+                        help="""Path to the file containing the activation
+                        times.""")
+
+    parser.add_argument('-p',
+                        '--AP-params',
+                        dest='ap_params',
+                        action='store',
+                        default=None,
+                        type=str,
+                        nargs='?',
+                        help="""Path to the file with the AP.""")
+
+
+
+
+    parser.add_argument('case_dir',
+                        action='store',
+                        type=str,
+                        nargs='?',
+                        help="""Path to a directory containing the ventricle_Tagged.vtk
+                        and the corresponding config files.""")
+
+
+
+
+
+    args = parser.parse_args()
+
+    if args.case_dir is not None:
+        if not os.path.exists(args.case_dir) or not os.path.isdir(args.case_dir):
+            print("ERROR: Given case_dir does not exist or is not a valid directory")
+            sys.exit()
+
+    if args.act_times is not None:
+        if not os.path.exists(args.act_times) or not os.path.isfile(args.act_times):
+            print("ERROR: Given act_times file does not exist or is not a file")
+            sys.exit()
+
+
+    ap_params = read_AP_json(path=args.ap_params, abs_path=True)
+    AT_to_AP(case_dir=args.case_dir,
+             ap_params=ap_params,
+             act_times_file=args.act_times,
+             f=args.f)
